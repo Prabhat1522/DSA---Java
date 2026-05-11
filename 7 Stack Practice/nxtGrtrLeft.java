ba169b10f26dc4b7676fbcd2a0c204d7d917b301
Prabhat kumar <prabhat844502@gmail.com>
2026-05-11 22:17:41 +0530

    nxtGrtrLeft stack problem

---
+++ 7 Stack Practice/nxtGrtrLeft.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class nxtGrtrLeft {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        int arr[] = {6, 8, 0, 1, 3};
+        int nxtGreaterleft[] =new int[arr.length];
+        for(int i=0;i<arr.length;i++) {
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+            if(s.isEmpty()) {
+                nxtGreaterleft[i] = -1;
+            } else {
+                nxtGreaterleft[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        for(int i=0;i<nxtGreaterleft.length;i++) {
+            System.out.print(nxtGreaterleft[i]+" ");
+        }
+        System.out.println();
+    }
+}
