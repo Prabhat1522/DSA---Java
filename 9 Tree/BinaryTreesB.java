abac459c211bf08aa2e089d58e766a50f16a5594
Prabhat kumar <prabhat844502@gmail.com>
2026-07-28 08:50:16 +0530

    Level Order traversal

--- 9 Tree/BinaryTreesB.java
+++ 9 Tree/BinaryTreesB.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class BinaryTreesB {
     static class Node {
         int data;
@@ -38,6 +39,31 @@ public class BinaryTreesB {
             postorder(root.right);
             System.out.print(root.data+" ");
         }
+        public static void levelOrder(Node root) {
+            if(root == null) return;
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()) {
+                Node currNode = q.remove();
+                if(currNode == null) {
+                    System.out.println();
+                    if(q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main(String args[]) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -45,10 +71,13 @@ public class BinaryTreesB {
         Node root = tree.buildTree(nodes);
         //System.out.println(root.data);
 
-        tree.preorder(root);
-        System.out.println();
-        tree.inorder(root);
-        System.out.println();
-        tree.postorder(root);
+        // tree.preorder(root);
+        // System.out.println();
+        // tree.inorder(root);
+        // System.out.println();
+        // tree.postorder(root);
+
+        tree.levelOrder(root);
+
     }
 }
